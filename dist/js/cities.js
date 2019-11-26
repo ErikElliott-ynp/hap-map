eefd857532924b02f6ed92760017d74d738d68c2
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-25 20:44:11 -0800

    add metros array

---
+++ dist/js/cities.js
@@ -0,0 +1,86 @@
+export const metros = [{
+        city: "NYC",
+        long: -70.1,
+        lat: 38.1
+    },
+    {
+        city: "LA",
+        long: -121.3,
+        lat: 30.5
+    },
+    {
+        city: "CHI",
+        long: -86.3,
+        lat: 39.7
+    },
+    {
+        city: "DAL",
+        long: -97.3,
+        lat: 28.3
+    },
+    {
+        city: "HOU",
+        long: -95.3,
+        lat: 25.5
+    },
+    {
+        city: "PHI",
+        long: -72,
+        lat: 37.2
+    },
+    {
+        city: "DC",
+        long: -74.4,
+        lat: 35.4 
+    },
+    {
+        city: "MIA",
+        long: -78.2,
+        lat: 23.8
+    },
+    {
+        city: "ATL",
+        long: -82,
+        lat: 29.8
+    },
+    {
+        city: "BOS",
+        long: -66.9,
+        lat: 40.2
+    },
+    {
+        city: "SF",
+        long: -126,
+        lat: 34.3
+    },
+    {
+        city: "PHO",
+        long: -114,
+        lat: 29.8
+    },
+    {
+        city: "DET",
+        long: -81,
+        lat: 40.1
+    },
+    {
+        city: "SEA",
+        long: -126.6,
+        lat: 46.3
+    },
+    {
+        city: "MIN",
+        long: -93,
+        lat: 43.3
+    },
+    {
+        city: "STL",
+        long: -89.6,
+        lat: 36
+    },
+    {
+        city: "DEN",
+        long: -106,
+        lat: 36.7
+    },
+]
--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,10 @@ let svg = d3.select("svg");
 
 let path = d3.geoPath();
 
+let proj = d3.geoAlbersUsa();
+
+import { metros } from "./js/cities"
+
 d3.json("https://d3js.org/us-10m.v1.json",  (error, us) => {
     if (error) throw error;
 
@@ -14,12 +18,21 @@ d3.json("https://d3js.org/us-10m.v1.json",  (error, us) => {
         .enter().append("path")
         .attr("d", path);
 
+    svg.append('path')
+        .attr('class', 'county-borders')
+        .attr('d', path(topojson.mesh(us, us.objects.counties, function (a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); })));
+        
     svg.append("path")
         .attr("class", "state-borders")
         .attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; })));
 
-    svg.append('path')
-        .attr('class', 'county-borders')
-        .attr('d', path(topojson.mesh(us, us.objects.counties, function (a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); })));
+    metros.forEach(met => {
+        svg.append("circle")
+            .attr("r", 5)
+            .attr("class", "nyc")
+            .attr("transform", function () { return "translate(" + proj([met.long, met.lat]) + ")"; });
+
+    })
+
 });
 
