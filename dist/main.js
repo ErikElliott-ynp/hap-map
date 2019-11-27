8e114476618d7bc4fc15766d28a9b1e5bc1c4d24
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-27 14:11:51 -0800

    attempt to add initial cities circles

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,39 @@
 import nationalParksGraph from "./js/nat_parks";
 import makeMapPlain from './js/map_plain';
+import { metros } from "./js/cities";
 
 console.log('And Here . . . we. . . . go');
 
 document.addEventListener("DOMContentLoaded", () => {
     makeMapPlain();
+    metros.forEach( met => {
+        d3.select('svg')
+            .append('circle')
+            .attr('r', 5)
+            .attr("fill", met.color)
+            .origin([met.long, met.lat])
+
+    })
+
 })
 
 // reset
-let reset = document.getElementById('reset');
-reset.addEventListener("click", () => {
+
+const resetMap = () => {
     d3.selectAll('.cube')
         .remove();
+}
+
+let reset = document.getElementById('reset');
+reset.addEventListener("click", () => {
+  resetMap();
 })
 
 // creates map with states and counties
 
 let btn = document.getElementById('main');
 btn.addEventListener('click', () => {
+    resetMap();
     nationalParksGraph();
 })
 
