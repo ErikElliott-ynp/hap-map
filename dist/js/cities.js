e5ae1b9d44c52b2acf87393393068e80dde48360
ErikElliottCreates <erikelliott92@gmail.com>
2019-12-01 11:28:05 -0800

    add dist string

--- dist/js/cities.js
+++ dist/js/cities.js
@@ -3,111 +3,143 @@ export const metros = [{
         long: 861,
         lat: 210,
         color: "blue",
-        dist: "300 mi"
+        distStr: "300 mi",
+        dist: 300,
+        hap: .7
     },
     {
         city: "la",
         long: 88,
         lat: 351,
         color: "purple",
-        dist: "130 mi"
+        distStr: "130 mi",
+        dist: 130,
+        hap: .75
     },
     {
         city: "chi",
         long: 626,
         lat: 224,
         color: "red",
-        dist: "60 mi"
+        distStr: "60 mi",
+        dist: 60,
+        hap: .43
     },
     {
         city: "dal",
         long: 467,
         lat: 445,
         color: "white",
-        dist: "290 mi"
+        distStr: "290 mi",
+        dist: 290,
+        hap: .33
     },
     {
         city: "hou",
         long: 500,
         lat: 510,
         color: "tan",
-        dist: "400 mi"
+        distStr: "400 mi",
+        dist: 400,
+        hap: .05
     },
     {
         city: "dc",
         long: 811,
         lat: 265,
         color: "grey",
-        dist: "80 mi"
+        distStr: "80 mi",
+        dist: 80,
+        hap: .55
     },
     {
         city: "mia",
         long: 805,
         lat: 560,
         color: "teal",
-        dist: "40 mi"
+        distStr: "40 mi",
+        dist: 40,
+        hap: .45
     },
     {
         city: "atl",
         long: 715,
         lat: 401,
         color: "yellow",
-        dist: "160 mi"
+        distStr: "160 mi",
+        dist: 160,
+        hap: .25
     },
     {
         city: "bos",
         long: 893,
         lat: 159,
         color: "green",
-        dist: "280 mi"
+        distStr: "280 mi",
+        dist: 280,
+        hap: .63
     },
     {
         city: "sf",
         long: 38,
         lat: 261,
         color: "orange",
-        dist: "100 mi"
+        distStr: "100 mi",
+        dist: 100,
+        hap: .90
     },
     {
         city: "pho",
         long: 199,
         lat: 390,
         color: "brown",
-        dist: "110 mi"
+        distStr: "110 mi",
+        dist: 110,
+        hap: .80
     },
     {
         city: "det",
         long: 700,
         lat: 207,
         color: "navy",
-        dist: "180 mi"
+        distStr: "180 mi",
+        dist: 180,
+        hap: .10
     },
     {
         city: "sea",
         long: 95,
         lat: 60,
         color: "pink",
-        dist: "60 mi"
+        distStr: "60 mi",
+        dist: 60,
+        hap: .85
     },
     {
         city: "min",
         long: 529,
         lat: 162,
         color: "fuchsia",
-        dist: "270 mi"
+        distStr: "270 mi",
+        dist: 270,
+        hap: 1.0
     },
     {
         city: "stl",
         long: 584,
         lat: 300,
         color: "aqua",
-        dist: "0 mi"
+        distStr: "0 mi",
+        dist: 0,
+        hap: .30
     },
     {
         city: "den",
         long: 340,
         lat: 280,
         color: "gold",
-        dist: "65 mi"
+        distStr: "65 mi",
+        dist: 65,
+        hap: .65
     },
 ]
